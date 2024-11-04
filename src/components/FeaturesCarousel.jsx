3c414cd4b310ce09148c7ac01b1ed6086d2d7017
pejuameenat <pejuameenat@gmail.com>
2024-11-04 23:59:40 +0100

    changed features apperanceo n mobile view

--- src/components/FeaturesCarousel.jsx
+++ src/components/FeaturesCarousel.jsx
@@ -14,12 +14,12 @@ const FeaturesCarousel = () => {
         <div className="bg-myCustomColor-light mx-auto mt-6 md:mt-14 py-5 px-5">
           <Slider {...sliderSettings}>
             {sliderImages.map(({url,image, id}) => (
-              <div key={id}>
+              <div key={id} className="ml-7 h-full">
                 <a href={url}target="_blank" rel="noopener noreferrer">
                   <img
                     src={image}
                     alt={`Slide ${id + 1}`}
-                    className="w-full"
+                    className="w-full h-inherit object-cover"
                   />
                 </a>
               </div>
