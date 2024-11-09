4635a88a5d04c49fb9e7a61d0c30eb25f1c69951
pejuameenat <pejuameenat@gmail.com>
2024-11-09 16:38:21 +0100

    form validation

--- src/store.js
+++ src/store.js
@@ -1,15 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import sliderReducer from './features/slider/sliderSlice';
-import modalReducer from './features/modal/modalSlice';
-import allSliderReducer from './features/allSliderComponent/allSliderSlice';
-import testimonialReducer from './features/testimonials/testimonialSlice'
-import windowReducer from './features/windowSlicer/windowSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import sliderReducer from "./features/slider/sliderSlice";
+import modalReducer from "./features/modal/modalSlice";
+import allSliderReducer from "./features/allSliderComponent/allSliderSlice";
+import testimonialReducer from "./features/testimonials/testimonialSlice";
+import windowReducer from "./features/windowSlicer/windowSlice";
+import formReducer from "./features/formSlicer/formSlice";
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
     modal: modalReducer,
     allSlider: allSliderReducer,
     testimonials: testimonialReducer,
-    windowSlice:windowReducer,
+    windowSlice: windowReducer,
+    form:formReducer,
   },
 });
